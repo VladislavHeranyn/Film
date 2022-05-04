896059467d31aafe446dcec065d761ecb36ea92d
login <vheranin@gmail.com>
2022-05-04 18:53:56 +0300

    film

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react"
+import "materialize-css"
+import { useDispatch } from "react-redux"
+import { Header } from "./components/header"
+// import { News } from "./actions/pages"
+import { Datel } from "./components/detal"
+import { Search } from "./components/resultSearch"
+import { List } from "./components//list"
+import { actionList } from "./actions/pages"
+import { useSelector } from "react-redux"
+
+
 
 function App() {
+
+  const dispatch = useDispatch()
+
+  const isAuth = useSelector(state => state.user)
+
+  console.log(isAuth.list)
+
+  useEffect(() => {
+    dispatch(actionList())
+  }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container" >
+
+      <Header />
+      {isAuth.detal && <Datel />}
+      {isAuth.search && <Search />}
+      {isAuth.list && <List />}
+
     </div>
   );
 }
 
 export default App;
+
+
+// import React from "react"
+// import { actionList } from "../actions/pages"
+// import { useDispatch } from "react-redux"
+
+// export const List = (props) => {
+
+//     const dispatch = useDispatch()
+
+//     return (
+//         <div className="navigation_mobile">
+//             <p onClick={() => dispatch(actionList())}>LIST</p>
+//         </div>)
+
+// }
+
